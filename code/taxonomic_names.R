691c5ba5adbb3572e4e15df154e234b7e2f52079
Marie Gutgesell <marie.gutgesell11@gmail.com>
2023-12-01 11:25:44 -0900

    fixed code to not remove higher level categories where present, should now be correct

--- code/taxonomic_names.R
+++ code/taxonomic_names.R
@@ -4,33 +4,21 @@ library(tidyverse)
 library(data.table)
 library(taxize)
 
+
 ##read in cleaned dataframe saved to intermediate files
 setwd("~/Desktop/Wild Foods Repo/")
 df <- read.csv("intermediate_files/harvest_data_clean.csv") 
 
-##lets see if can sort unknowns, want to remove the unknown part
-df_test <- df %>%
-  filter(grepl("Unknown", Species))
-
-test <- df %>%
-  filter(Site_Year_Code == "Angoon_1987")
 
 ##going to try to see if just removing the unknown works, so is just lowest taxonomic name
-df$Species_2 <- str_remove(df$Species, "Unknown ")
-
-test <- df %>%
-  filter(Family == "Eel")
-##no lamprey caught, were in db but was 0
-
-test2 <- df %>%
-  filter(Species == "Scallop") 
+df$Taxa_lvl5_b <- str_remove(df$Taxa_lvl5, "Unknown ")
 
 ##unknown large land mammals -- only in Haines 1983, there are # harvested, but no estimated harvest weights because there is no conversion unit... 
 ##there may be certain things like this we can't get around..but this does somehow need to be taken into account when comparing food webs... 
 ##create species list
 
 sp_list <- df %>%
-  distinct(General_Category, General_Category_lvl2, Family, Species, Species_2, Habitat) 
+  distinct(Taxa_lvl1, Taxa_lvl2, Taxa_lvl3, Taxa_lvl4, Taxa_lvl5_b, Habitat) 
 
 
 ##I think Sea bass are the same as black rockfish
