c3ec5865277f837b8aacffab0b79508be4aba6a9
Marie Gutgesell <marie.gutgesell11@gmail.com>
2024-05-31 10:56:06 -0800

    cleaning out old scripts

--- README.md
+++
@@ -1,2 +0,0 @@
-# Wild_Foods
-Repository for data and code associated with Wild Foods Project in SE Alaska
--- code/5T_harvest_structures_comm_avg.R
+++
@@ -1,5 +0,0 @@
-##Generate Harvest Structures for Each Community (averaged across years) 
-
-##source code that calculates average harvest across time for each community
-source("code/3T_calculate_avg_community_harvest.R")
-rm(list = ls()[!ls() %in% c("df_comm_avg")])
